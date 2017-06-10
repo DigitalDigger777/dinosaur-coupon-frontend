834a935fbf52233e3bfd4803b3d69f225cb601d6
korman <korman.yuri@gmail.com>
2017-06-10 18:15:44 +0300

    added coupon sent

--- src/js/components/store/StoreList.js
+++ src/js/components/store/StoreList.js
@@ -2,9 +2,10 @@
  * Created by korman on 03.06.17.
  */
 
+import Config from '../Config';
 import React from 'react';
-import {Link} from 'react-router-dom';
 import axios from 'axios';
+import StoreListItem from './StoreListItem';
 
 export default class StoreList extends React.Component {
     constructor(props){
@@ -16,17 +17,19 @@ export default class StoreList extends React.Component {
     }
 
     componentDidMount(props){
-        axios.get('http://coupon-backend.ppcgclub.com/coupon/shopper/rest/0', {
+        const config = new Config();
+
+        axios.get(config.baseUrl + 'coupon/shopper/rest/0', {
             params: {
                 method:'LIST',
                 page:1,
                 items_on_page:5
             }
         }).then(response => {
-            console.log(response.data.items);
+            //console.log(response.data.items);
             this.setState({countPages:response.data.count_pages});
             this.setState({items: response.data.items});
-            console.log(this.state);
+            //console.log(this.state);
         }).catch(function(error){
             console.log(error);
         });
@@ -34,30 +37,12 @@ export default class StoreList extends React.Component {
 
     render(){
         if (this.state.items.length > 0) {
+            console.log(this.state.items);
             return (
                 <div>
                     {
-                        this.state.items.map(item =>
-                            <div key={item.id} className="zan-card zan-card-1 zan-container-content">
-                                <Link to={`store/${item.id}`}>
-                                    <img className="preload-image" data-original="images/zan-images/shop-logo.gif"
-                                         alt="img"
-                                         src="images/pictures/1t.jpg" style={{display: 'block'}}/>
-                                    <div className="zan-wrap-content">
-                                        <strong>{item.name}</strong>
-                                        <p>&nbsp;</p>
-                                        <p>
-                                            <i className="fa">
-                                                <img className="footer-menu-icon" src="images/zan-icon/coupon.png"
-                                                     width="24"
-                                                     height="24" alt=""/>
-                                            </i>
-                                            <span className="qty">5</span>
-                                            <i className="fa fa-chevron-right" aria-hidden="true"></i>
-                                        </p>
-                                    </div>
-                                </Link>
-                            </div>
+                        this.state.items.map((item, index) =>
+                            <StoreListItem key={index} item={item} />
                         )
                     }
                 </div>
---
+++ src/js/components/store/StoreListItem.js
@@ -0,0 +1,53 @@
+/**
+ * Created by korman on 03.06.17.
+ */
+
+import Config from '../Config';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+export default class StoreListItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            item: props.item
+        }
+        console.log(props);
+    }
+
+    render(){
+         const config = new Config();
+         let logo = <img className="preload-image" data-original="images/zan-images/shop-logo.gif"
+                          alt="img"
+                          src="images/pictures/1t.jpg" style={{display: 'block'}}/>;
+         if (this.state.item.logo) {
+
+             logo = <img className="preload-image" data-original={`${config.baseImagePath}images/logo/${this.state.item.logo}`}
+                               alt="img"
+                               src="images/pictures/1t.jpg" style={{display: 'block'}}/>;
+         }
+
+         return(
+             <div className="zan-card zan-card-1 zan-container-content">
+                 <Link to={`store/${this.state.item.id}`}>
+                     {logo}
+
+                     <div className="zan-wrap-content">
+                         <strong>{this.state.item.name}</strong>
+                         <p>&nbsp;</p>
+                         <p>
+                             <i className="fa">
+                                 <img className="footer-menu-icon" src="images/zan-icon/coupon.png"
+                                      width="24"
+                                      height="24" alt=""/>
+                             </i>
+                             <span className="qty">5</span>
+                             <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                         </p>
+                     </div>
+                 </Link>
+             </div>
+         );
+    }
+}
