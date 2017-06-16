93161c071c3dd30a3b7e85fe0c1e6e18dfcb8a4b
korman <korman.yuri@gmail.com>
2017-06-16 23:32:06 +0300

    fixed

--- src/js/index.js
+++ src/js/index.js
@@ -61,57 +61,61 @@ export default class Index extends React.Component{
             if (this.state.url != '') {
                 window.location = this.state.url;
             }
+            return(
+                <div></div>
+            );
+        } else {
+            if (user != null) {
+                let config = new Config();
+
+                axios.post(config.baseUrl + 'coupon/consumer/rest/0', {
+                    socialId: user.unionid,
+                    socialDataProfile: user
+                }).then(result => {
+
+                    window.localStorage.setItem('user_id', result.data.id);
+                    window.localStorage.setItem('user', JSON.stringify(user));
+                    window.location = '/';
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+
+            return (
+                <HashRouter history={hashHistory}>
+                    <div>
+                        <Route exact path="/coupon/coupons/list" component={BodyCoupon} />
+                        <Route exact path="/" component={BodyCoupon} />
+                        <Route exact path="/coupon/friend/list" component={BodyCoupon} />
+                        <Route exact path="/coupon/:id" component={CouponDetail} />
+                        <Route exact path="/friend/coupon/:id" component={CouponFriendDetail} />
+                        <Route exact path="/friend/coupon/:id/:issuedCouponId/:ownerUserId" component={CouponFriendDetail} />
+
+                        <Route exact path="/stores" component={BodyStore} />
+                        <Route exact path="/store/:id" component={StoreDetail} />
+                        <Route exact path="/store/all-coupons/:id" component={StoreDetail} />
+                        <Route exact path="/store/your-coupons/:id" component={StoreDetail} />
+
+                        <Route exact path="/profile" component={Profile} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/login-wechat" component={LoginWechat} />
+                        {/*<Route exact path="/event/:page" component={BodyEvent}></Route>*/}
+                        {/*<Route exact path="/coupon/:page" component={BodyCoupon}></Route>*/}
+                        {/*<Route exact path="/event/detail/:id/:tab" component={BodyEventDetail}></Route>*/}
+                        {/*<Route exact path="/event/detail-friend/:id/:tab" component={BodyEventDetailFriend}></Route>*/}
+
+                        {/*<Route path="/coupon/detail/:id" component={CouponDetail}></Route>*/}
+                        {/*<Route exact path="/coupon/redeem/:id" component={CouponRedeem}></Route>*/}
+                        {/*<Route exact path="/login" component={Login}></Route>*/}
+                    </div>
+                </HashRouter>
+            )
         }
 
-        if (user != null) {
-            let config = new Config();
 
-            axios.post(config.baseUrl + 'coupon/consumer/rest/0', {
-                socialId: user.unionid,
-                socialDataProfile: user
-            }).then(result => {
-
-                window.localStorage.setItem('user_id', result.data.id);
-                window.localStorage.setItem('user', JSON.stringify(user));
-                window.location = '/';
-            }).catch(error => {
-                console.log(error);
-            });
-
-
-
-        }
         //console.log('root');
 
-        return (
-            <HashRouter history={hashHistory}>
-                <div>
-                    <Route exact path="/coupon/coupons/list" component={BodyCoupon} />
-                    <Route exact path="/" component={BodyCoupon} />
-                    <Route exact path="/coupon/friend/list" component={BodyCoupon} />
-                    <Route exact path="/coupon/:id" component={CouponDetail} />
-                    <Route exact path="/friend/coupon/:id" component={CouponFriendDetail} />
-                    <Route exact path="/friend/coupon/:id/:issuedCouponId/:ownerUserId" component={CouponFriendDetail} />
 
-                    <Route exact path="/stores" component={BodyStore} />
-                    <Route exact path="/store/:id" component={StoreDetail} />
-                    <Route exact path="/store/all-coupons/:id" component={StoreDetail} />
-                    <Route exact path="/store/your-coupons/:id" component={StoreDetail} />
-
-                    <Route exact path="/profile" component={Profile} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/login-wechat" component={LoginWechat} />
-                    {/*<Route exact path="/event/:page" component={BodyEvent}></Route>*/}
-                    {/*<Route exact path="/coupon/:page" component={BodyCoupon}></Route>*/}
-                    {/*<Route exact path="/event/detail/:id/:tab" component={BodyEventDetail}></Route>*/}
-                    {/*<Route exact path="/event/detail-friend/:id/:tab" component={BodyEventDetailFriend}></Route>*/}
-
-                    {/*<Route path="/coupon/detail/:id" component={CouponDetail}></Route>*/}
-                    {/*<Route exact path="/coupon/redeem/:id" component={CouponRedeem}></Route>*/}
-                    {/*<Route exact path="/login" component={Login}></Route>*/}
-                </div>
-            </HashRouter>
-        )
     }
 }
 ReactDOM.render(<Index/>,document.getElementById('page-transitions'));
