038d36f5a42fd25a2a4ef2a180ea0047c743e31e
korman <korman.yuri@gmail.com>
2017-06-18 02:27:50 +0300

    added coupon sent

--- src/js/components/coupon/CouponFriendDetail.js
+++ src/js/components/coupon/CouponFriendDetail.js
@@ -67,8 +67,8 @@ export default class CouponFriendDetail extends React.Component {
         wx.config({
             debug: true, // Enables debugging mode. Return values of all APIs called will be shown on the client. To view the sent parameters, open the log view of developer tools on a computer browser. The parameter information can only be printed when viewed from a computer.
             appId: config.weChatConfig.oa_appid, // Required, unique identifier of the official account
-            timestamp: '1497740493', // Required, timestamp for the generated signature
-            nonceStr: '914STrojLMcXrtuc', // Required, random string for the generated signature
+            timestamp: '1497741995', // Required, timestamp for the generated signature
+            nonceStr: 'yqa4OiNtI0yeOHh2', // Required, random string for the generated signature
             signature: config.weChatConfig.signature, // Required, signature. See Appendix 1.
             jsApiList: [
                 'onMenuShareTimeline',
