d28cc948cfcc03f7e07f5b01c34a5bac5eaf0419
korman <korman.yuri@gmail.com>
2017-07-04 19:18:55 +0300

    added menu active

--- src/js/components/parts/Menu.js
+++ src/js/components/parts/Menu.js
@@ -6,13 +6,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Menu extends React.Component {
+    constructor(props){
+        super(props);
+
+        console.log('menu', window.location.hash);
+        this.state = {
+            active: 'coupon',
+            activeCoupon: '',
+            activeStores: '',
+            activeMessages: ''
+        }
+    }
+
+    componentWillMount(){
+
+        switch (window.location.hash) {
+            case '#/':
+                    this.state.activeCoupon = 'zan-active';
+                break;
+            case '#/coupon/friend/list':
+                    this.state.activeStores = 'zan-active';
+                break;
+            case '#/stores':
+                    this.state.activeStores = 'zan-active';
+                break;
+            case '#/messages':
+                    this.state.activeMessages = 'zan-active';
+                break;
+        }
+
+        console.log('mmm', this.state);
+    }
 
     render(){
+
         return (
             <div className="footer-menu footer-menu-light">
-                <Link to="/"><i className="fa zan-coupon zan-active"></i>Coupons</Link>
-                <Link to="/stores"><i className="fa zan-store"></i>Stores</Link>
-                <Link to="/messages"><i className="fa zan-message"></i>Messages</Link>
+                <Link to="/"><i className={`fa zan-coupon ${this.state.activeCoupon}`}></i>Coupons</Link>
+                <Link to="/stores"><i className={`fa zan-coupon ${this.state.activeStores}`}></i>Stores</Link>
+                <Link to="/messages"><i className={`fa zan-coupon ${this.state.activeMessages}`}></i>Messages</Link>
                 {/*<Link to="/profile"><i className="fa zan-coupon"></i>Profile</Link>*/}
 
                 {/*<a href="main.html" className="zan-active" ><i className="fa zan-coupon"></i>Coupons</a>*/}
